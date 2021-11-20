a62097e619faf38f5e22c88e613d47f5942cba1b
Sina Ghasemi <itsjustsina@gmail.com>
2021-11-20 18:26:25 +0330

    Bishop moves added.

--- src/Model/Bishop.java
+++ src/Model/Bishop.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Bishop extends Piece {
     public Bishop(char color, int x, int y) {
@@ -13,6 +14,68 @@ public class Bishop extends Piece {
 
     @Override
     public ArrayList<ArrayList<Integer>> possibleMoves() {
-        return null;
+        ArrayList<ArrayList<Integer>> possibleMoves = new ArrayList<>();
+        Piece piece;
+        for (int i = this.getX()+1; i <= 8 ; i++) {
+            int posY = this.getY()+this.getX()-i;
+            if (posY >= 1){
+                piece = gameController.checkForPiece(i, posY);
+                if(piece == null){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                } else if(gameController.isEnemy(piece)){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                    break;
+                } else if(!gameController.isEnemy(piece)){
+                    break;
+                }
+            }
+        }
+
+        for (int i = this.getX()-1; i >= 1 ; i--) {
+            int posY = this.getY()- (this.getX()-i);
+            if (posY >= 1){
+                piece = gameController.checkForPiece(i, posY);
+                if(piece == null){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                } else if(gameController.isEnemy(piece)){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                    break;
+                } else if(!gameController.isEnemy(piece)){
+                    break;
+                }
+            }
+        }
+
+        for (int i = this.getX()-1; i >= 1 ; i--) {
+            int posY = this.getY() + (this.getX()-i);
+            if (posY <= 8){
+                piece = gameController.checkForPiece(i, posY);
+                if(piece == null){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                } else if(gameController.isEnemy(piece)){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                    break;
+                } else if(!gameController.isEnemy(piece)){
+                    break;
+                }
+            }
+        }
+
+        for (int i = this.getX()+1; i <= 8 ; i++) {
+            int posY = this.getY()+ (i - this.getX());
+            if (posY <= 8){
+                piece = gameController.checkForPiece(i, posY);
+                if(piece == null){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                } else if(gameController.isEnemy(piece)){
+                    possibleMoves.add(new ArrayList<>(Arrays.asList(i, posY)));
+                    break;
+                } else if(!gameController.isEnemy(piece)){
+                    break;
+                }
+            }
+        }
+
+        return possibleMoves;
     }
 }
