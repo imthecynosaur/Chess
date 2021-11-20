9755e57405455772963148924ebaabe9991f6ceb
Sina Ghasemi <itsjustsina@gmail.com>
2021-11-20 19:09:24 +0330

    king moves added.

--- src/Model/King.java
+++ src/Model/King.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class King extends Piece {
     public King(char color, int x, int y) {
@@ -13,6 +14,23 @@ public class King extends Piece {
 
     @Override
     public ArrayList<ArrayList<Integer>> possibleMoves() {
-        return null;
+        ArrayList<ArrayList<Integer>> possibleMoves = new ArrayList<>();
+        Piece piece;
+        for (int j = this.getY()-1; j <= this.getY()+1 ; j++) {
+            if(j >= 1 && j <= 8){
+                for (int i = this.getX()-1; i <= this.getX()+1 ; i++) {
+                    if(i >= 1 && i <= 8){
+                        piece = gameController.checkForPiece(i, j);
+                        if(piece == null){
+                            possibleMoves.add(new ArrayList<>(Arrays.asList(i, j)));
+                        } else if(gameController.isEnemy(piece)){
+                            possibleMoves.add(new ArrayList<>(Arrays.asList(i, j)));
+                        }
+                    }
+                }
+            }
+
+        }
+        return possibleMoves;
     }
 }
